52246c6eb5e1debc5d42490220a6af8030dc4275
504311708@qq.com <504311708@qq.com>
2025-03-05 17:11:42 +0800

    tRPC configuration

---
+++ docs/07 tRPC configuration.md
@@ -0,0 +1,18 @@
+# tRPC configuration
+
+## Enable transformer on tRPC
+
+```sh
+pnpm add react-error-boundary@5.0.0
+
+```
+
+## Add auth to tRPC context
+
+## Add protectedProcedure
+
+## Add rate limiting
+
+```sh
+pnpm add @upstash/redis@1.34.3 @upstash/ratelimit@2.0.5
+```
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,16 @@
 import { z } from 'zod'
-import { baseProcedure, createTRPCRouter } from '../init'
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init'
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query((opts) => {
+      console.log(opts.ctx.user)
       return {
-        greeting: `hello ${opts.input.text}`,
+        greeting: `hello ${opts.ctx.user.name}`,
       }
     }),
 })
