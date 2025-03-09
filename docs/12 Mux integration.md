820622200f9dbabeae140f95123ac9f0713ef4c8
504311708@qq.com <504311708@qq.com>
2025-03-10 00:34:37 +0800

    mux integration

---
+++ docs/12 Mux integration.md
@@ -0,0 +1,17 @@
+# Mux integration
+
+## Create a responsive dialog
+
+## Create a free Mux account
+
+### Mux free account limitations
+
+1. length limit (30s)
+2. Video deletion after 24h
+3. Mux watermark
+
+## Get a 15s video with english audio
+
+https://tinyurl.com/newtube-clip
+
+## Create upload modal
--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,10 +1,12 @@
 'use client'
+import { ResponsiveModal } from '@/components/responsive-modal'
 import { Button } from '@/components/ui/button'
 import { trpc } from '@/trpc/client'
 import { error } from 'console'
 
 import { Loader2Icon, PlusIcon } from 'lucide-react'
 import { toast } from 'sonner'
+import { StudioUploader } from './studio-uplodaer'
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils()
@@ -18,16 +20,28 @@ export const StudioUploadModal = () => {
     },
   })
   return (
-    <Button
-      variant="secondary"
-      disabled={create.isPending}
-      onClick={() => create.mutate()}>
-      {create.isPending ? (
-        <Loader2Icon className="animate-spin" />
-      ) : (
-        <PlusIcon />
-      )}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Uploader"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}>
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon />
+        )}
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        disabled={create.isPending}
+        onClick={() => create.mutate()}>
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
   )
 }
