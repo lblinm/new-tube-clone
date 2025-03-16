094694a3bf3247bc75a2ef184b2c13779a92197d
504311708@qq.com <504311708@qq.com>
2025-03-16 16:44:45 +0800

    video reactions

---
+++ docs/20 Video reactions.md
@@ -0,0 +1,9 @@
+# Video reactions
+
+## Create video reactions schema
+
+## Combine video reactions for "getOne" videos procedures
+
+## Create video reactions like & dislike procedure
+
+## Connect VideoReactions component with new API
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { createTRPCRouter } from '../init'
 import { categoriesRouter } from '@/modules/categories/server/procedures'
 import { videosRouter } from '@/modules/videos/server/procedure'
 import { videoViewsRouter } from '@/modules/video-views/server/procedure'
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedure'
 export const appRouter = createTRPCRouter({
   videos: videosRouter,
   studio: studioRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 })
 // export type definition of API
 export type AppRouter = typeof appRouter
